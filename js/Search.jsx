06cec9cdc60652d0514730843a575b7e27ecfdf2
davis008 <davisomondi008@gmail.com>
2020-05-15 13:29:45 +0300

    innitial commit-data will be changed to educational data and more

---
+++ js/Search.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import ShowCard from './ShowCard';
+import preload from '../data.json';
+
+class Search extends Component {
+  state = {
+    searchTerm: ''
+  };
+  handleSearchTermChange = event => {
+    this.setState({ searchTerm: event.target.value });
+  };
+  render() {
+    return (
+      <div className="search">
+        <header>
+          <h1>DigiElimu</h1>
+          <input
+            onChange={this.handleSearchTermChange}
+            value={this.state.searchTerm}
+            type="text"
+            placeholder="Search"
+          />
+        </header>
+        <div>
+          {preload.shows
+            .filter(
+              show =>
+                `${show.title} ${show.description}`.toUpperCase().indexOf(this.state.searchTerm.toUpperCase()) >= 0
+            )
+            .map(show => <ShowCard key={show.imdbID} {...show} />)}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Search;
